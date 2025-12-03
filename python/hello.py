3c935bc92bef93c695e411dfa8247b7709896bbc
Arnav Gupta <guptaarnav721@gmail.com>
2025-12-03 18:28:01 +0530

    Update hello.py

--- python/hello.py
+++ python/hello.py
@@ -29,3 +29,23 @@ elif num3>num1:
     print(num3)
 else:
     print("All numbers are equal")
+# frinting only even numbers
+for i in range(0,51):
+    if(i%2==0):
+        print(i)
+#printing the table of 10
+for i in range (0,11):
+    print(10,'x',i,'=',10*i)
+#for printing number in reverse
+for i in range(10,0,-1):
+    print(i)
+#defining a function
+def add(x,y):
+    return x+y
+result=add(10,1)
+print(result)
+#giving multiple results
+def calc(x,y):
+    return x+y,x*y
+add,mul=calc(10,5)
+print(add,mul)
