f96b6a137f4f79558a586f067c7254f357e979e0
Arnav Gupta <guptaarnav721@gmail.com>
2025-12-07 11:54:14 +0530

    Update project1_atm.py

--- project1_atm.py
+++ project1_atm.py
@@ -1,34 +1,33 @@
-balance=200000
-pin=2006
+balance = 200000
+pin = 2006
 print("Welcome to the ATM")
-checkpoint1=int(input("enter ATM pin: "))
-if checkpoint1 != pin:
-    print("Invalid pin")
-else:
-    if True:
+while True:
+    checkpoint1 = int(input("Enter PIN: "))
+    if checkpoint1 != pin:
+        print("Invalid PIN")
+        break
+    else:
+        print("Login Successful")
         print("1. Check Balance")
         print("2. Deposit Money")
         print("3. Withdraw Money")
         print("4. Exit")
-checkpoint2=int(input("Enter the choice"))
-if checkpoint2 == 1:
+        checkpoint2 = int(input("Enter the choice: "))
+        if checkpoint2 == 1:
             print("Your Balance:", balance)
-elif checkpoint2 == 2:
+        elif checkpoint2 == 2:
             amount = int(input("Enter amount to deposit: "))
             balance += amount
-            print(" Money Deposited")
-            print("New Balance:", balance)
-
-elif checkpoint2 == 3:
+            print("Deposit Successful! New Balance:", balance)
+        elif checkpoint2 == 3:
             amount = int(input("Enter amount to withdraw: "))
             if amount > balance:
                 print("Insufficient Balance!")
             else:
                 balance -= amount
-                print("Withdrawal Successful")
-                print("Remaining Balance:", balance)
-
-elif checkpoint2 == 4:
-    print("Thank you for using the ATM!")
-else:
-            print("Invalid choice! Please select again.")
+                print("Withdraw Successful! Remaining:", balance)
+        elif checkpoint2 == 4:
+            print("Thank you for using the ATM!")
+            break
+        else:
+            print("Invalid choice!")
